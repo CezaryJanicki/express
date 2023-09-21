68db47916bffdb33ea2e6ebb1445c74f8a4e729a
CezaryJanicki <cezary.janicki@gmail.com>
2023-09-21 15:36:21 +0200

    Add dark.hbs

--- server.js
+++ server.js
@@ -3,45 +3,33 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {layout: 'dark', name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: 'main' });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: 'dark' });
 });
 
-app.get('/history', (req, res, next) => {
-  res.show('history.html');
-});
-
-app.get('/hello/:name', (req, res, next) => {
-  res.send('Hello ' + req.params.name);
-});
-
-app.get('/hi/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'main' });
 });
 
 app.use((req, res) => {
