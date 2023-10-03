49b103580742b827b31bc0071a2fd4aeca31e662
CezaryJanicki <cezary.janicki@gmail.com>
2023-10-03 13:49:16 +0200

    Add send-message endpoint

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -30,6 +32,17 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history', { layout: 'main' });
+});
+
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.send('The message has been sent!');
+  }
+  else {
+    res.send('You can\'t leave fields empty!')
+  }
 });
 
 app.use((req, res) => {
